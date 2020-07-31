88f7bd238afbdd4958a2e8a5c42d5cc56236c293
Samay Verma <samay1231998@gmail.com>
2020-07-31 22:34:02 +0530

    change port to match heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express"),
     app = express(),
-    port = 3000,
+    port = process.env.PORT || 3000,
     between = require("./functions/between.js"),
     findById = require("./functions/findById"),
     findByTag = require("./functions/findByTag"),
