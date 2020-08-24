62cf7d7a5dd7465b7335167f7dbf472d50157eeb
samay verma <samayverma@samays-MacBook-Pro.local>
2020-08-24 20:00:37 +0530

    Fix CORS errors

--- src/api.js
+++ src/api.js
@@ -38,6 +38,14 @@ const findByTag = function(tag) {
 
 //////////////////////////////////////////Routes//////////////////////////////////////////
 
+//f*&# CORS
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Get a random excuse
 app.get('/random', (req, res) => {
     let excuse = findById(between(1, 100));
